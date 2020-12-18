db66c09642ad7a467aaa44d6209d261b1f1a553d
klabmam28 <76193958+klabmam28@users.noreply.github.com>
2020-12-18 12:15:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'l9q0J5HyCl1aWn6xZxF/TU+yPVsT98O+qPQrnoEl6w3jgwnZfW4JSG7NuGJcayDFiymptLAc70Fi5rTCCpdsLvGtut/pXB2x+DuMn3RviP3aPOxJ0thOwbo0jj3/3PsoWw396iKTEDv5k1hgAeZnwwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
